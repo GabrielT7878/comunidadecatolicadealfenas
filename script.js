bb710c4115e4f3a9cbd54babd661e805c0db53a6
Gabriel.T <gabrielfernandesthiago@hotmail.com>
2022-09-12 09:27:58 -0300

    api da liturgia teste

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 //menu-mobile
+function fazGet(url) {
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
+
 let menuBtn = document.querySelector('#menu-mobile');
 let menuStickers = document.querySelectorAll('.menu-sticker');
 let menuUl = document.querySelector('.ul-nav');
@@ -22,6 +29,22 @@ window.addEventListener('scroll',()=>{
             menuUl.classList.toggle('active');
         }
 })
+
+let data = fazGet('http://liturgia.herokuapp.com');
+let dataJson = JSON.parse(data);
+
+let main = document.querySelector('.main-cards');
+let card = document.createElement('div');
+let cardTitle = document.createElement('h2');
+cardTitle.appendChild(document.createTextNode("Oração do dia"));
+card.classList.add('card');
+var ret = dataJson.dia.replace('Oração do dia','');
+text = document.createTextNode(ret);
+card.appendChild(cardTitle);
+card.appendChild(text);
+main.appendChild(card);
+
+console.log(dataJson);
 
 
 
